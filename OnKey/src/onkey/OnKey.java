f57013b07a839ec8c03052651dfc04de4d3e7ad3
ratul <13ratul@gmail.com>
2013-08-03 11:38:25 +0600

    adding some features: 1. interactive UI. 2. non-focused window for writing on focused window. 3. robot class is used for key's value.

---
+++ OnKey/src/onkey/OnKey.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package onkey;
+
+/**
+ *
+ * @author RATUL
+ */
+public class OnKey {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new KeyBoard().setVisible(true);
+    }
+}
---
+++ OnKey/src/onkey/Sound.java
@@ -0,0 +1,23 @@
+package onkey;
+
+import javax.sound.sampled.*;
+import java.io.*;
+
+public class Sound
+{
+	public void play(String path) {//throws LineUnavailableException, UnsupportedAudioFileException, IOException
+            try{
+                File f = new File(path);
+                Clip clip=AudioSystem.getClip();
+                AudioInputStream ais=AudioSystem.getAudioInputStream(f);
+                clip.open(ais);
+                clip.loop(0);
+                //JOptionPane.showMessageDialog(null,"Close to exit");
+                //clip.close();
+            }
+            catch(Exception e){
+                System.out.println(e);
+            }
+	}
+        
+}
